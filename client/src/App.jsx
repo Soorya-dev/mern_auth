8e01c3e47401026961d81cc635f45db891e46313
Soorya dev <sooryadevanikkat@gmail.com>
2024-12-26 21:30:24 +0530

    added pages,routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,21 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Signin from './pages/Signin';
+import Signup from './pages/Signup';
+import Profile from './pages/Profile';
+import About from './pages/About';
+
 const App = () => {
   return (
-    <div className="text-red-900 p-4 rounded-lg">
-  Hello, Tailwind!
-</div>
-
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
